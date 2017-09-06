c26e2453581f7d7c08c8c9a0632cb69d6b5892d9
xzxie <86017516@qq.com>
2017-09-07 00:24:49 +0800

    第一个demo
    Signed-off-by: xzxie <86017516@qq.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # springboot-demo
 for springboot test
+
+### 1.添加pom ###
+### 2.运行App main ###
--- src/main/java/com/springboot/project/App.java
+++ src/main/java/com/springboot/project/App.java
@@ -1,13 +1,24 @@
 package com.springboot.project;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
 public class App 
 {
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "hello, 你好！";
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+    	
+    	SpringApplication.run(App.class, args);
     }
 }
